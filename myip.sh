8f1911adc21708ac03b16fc0868089805b3ddd40
dada <dada@LM18point1>
2018-10-31 15:26:01 -0700

    adjusted the comments to clean up file.

--- myip.sh
+++ myip.sh
@@ -1,8 +1,7 @@
 ! /bin/sh
-#to run as a cron script save to /etc/cron.hourly(weekly etc..set correct email
+#run in /etc/cron.hourly(weekly etc..set correct email
 currentIp=$(curl ipinfo.io/ip)
 oldIp="$(tail /home/dada/Programming/myip/currentIp.csv)"
-#echo currentIp $currentIp oldIp $oldIp
     if [ "$currentIp" != "$oldIp" ];
         then 
             echo $currentIp > /home/dada/Programming/myip/currentIp.csv
